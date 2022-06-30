78b6e2a21122c397395475432f17e3e7fe212cfa
Scott Mackenzie <scott.mac.web@gmail.com>
2022-06-30 17:24:24 +1200

    feat: .files init.vim sync

--- install.ps1
+++ install.ps1
@@ -1,4 +1,18 @@
 #scoop
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 irm get.scoop.sh | iex
-scoop bucket add extras; scoop bucket add versions
+scoop bucket add extras; scoop bucket add versions;
+
+scoop install 7zip git fd ripgrep zig gcc nvm gh draw.io obsidian omnisharp oh-my-posh pandoc;
+
+Install-Module posh-git;
+Install-Module PSReadLine -Scope CurrentUser;
+Install-Module Terminal-Icons -Scope CurrentUser
+
+
+# vim
+iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
+    ni $HOME/vimfiles/autoload/plug.vim -Force
+
+cd ~; mkdir AppData\Local\nvim;
+cp .\init.vim ~\AppData\Local\nvim\
